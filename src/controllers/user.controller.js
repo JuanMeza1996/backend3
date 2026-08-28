269d1077e4d170d996275cd45d769a3c2a4b5b48
JuanMeza1996 <159964320+JuanMeza1996@users.noreply.github.com>
2026-08-28 09:01:18 -0300

    Actualizar user.controller.js
    User controller actualizado

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -3,21 +3,21 @@ import { UserService } from '../services/user.service.js';
 const userService = new UserService();
 
 export class UserController {
-    async getUsers(req, res) {
+    async getUsers(req, res, next) {
         try {
             const users = await userService.getAllUsers();
             res.status(200).json({ status: 'success', payload: users });
         } catch (error) {
-            res.status(500).json({ status: 'error', message: error.message });
+            next(error);
         }
     }
 
-    async createUser(req, res) {
+    async createUser(req, res, next) {
         try {
             const newUser = await userService.registerUser(req.body);
             res.status(201).json({ status: 'success', payload: newUser });
         } catch (error) {
-            res.status(400).json({ status: 'error', message: error.message });
+            next(error);
         }
     }
 }
