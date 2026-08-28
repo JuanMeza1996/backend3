f509c6affefcd127f712d56cbbdd854300b0e7fe
JuanMeza1996 <159964320+JuanMeza1996@users.noreply.github.com>
2026-08-28 11:17:17 -0300

    Actualizar logger.routes.js
    Actualizacion logger

--- src/routes/logger.routes.js
+++ src/routes/logger.routes.js
@@ -3,6 +3,16 @@ import { logger } from '../utils/logger.js';
 
 const router = Router();
 
+/**
+ * @openapi
+ * /api/logger-test:
+ *   get:
+ *     summary: Probar los niveles de logger (Winston)
+ *     tags: [Logger]
+ *     responses:
+ *       200:
+ *         description:Logs emitidos correctamente.
+ */
 router.get('/', (req, res) => {
   logger.debug('Log de prueba: DEBUG');
   logger.http('Log de prueba: HTTP');
@@ -10,8 +20,7 @@ router.get('/', (req, res) => {
   logger.warning('Log de prueba: WARNING');
   logger.error('Log de prueba: ERROR');
   logger.fatal('Log de prueba: FATAL');
-
-  res.json({ status: 'success', message: 'Logs generados exitosamente' });
+  res.json({ status: 'success', message: 'Logger test ejecutado' });
 });
 
 export default router;
